6c3dd7c591ca30dfbe8ce9221e29a29d842cd4bd
gabrielbcruz99 <43832508+gabrielbcruz99@users.noreply.github.com>
2019-03-27 22:47:08 -0300

    Add files via upload

---
+++ precache-manifest.da694b2d028ca77762f2749110a92fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4fb40f21733606f04bd7",
+    "url": "/static/js/main.3add6521.chunk.js"
+  },
+  {
+    "revision": "f181ab7e26b0fc709d0a",
+    "url": "/static/js/2.4f1e9eba.chunk.js"
+  },
+  {
+    "revision": "4fb40f21733606f04bd7",
+    "url": "/static/css/main.70e8cd0d.chunk.css"
+  },
+  {
+    "revision": "f9b9191e868c0b039758658deb8cc5da",
+    "url": "/index.html"
+  }
+];
